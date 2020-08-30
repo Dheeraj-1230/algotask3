5caa3dbe447ab8ef4fe94071875d411b0b3740ec
Dheeraj-1230 <63278930+Dheeraj-1230@users.noreply.github.com>
2020-08-30 18:22:30 +0530

    updated

--- task3_A.cpp
+++ task3_A.cpp
@@ -2,21 +2,29 @@
 using namespace std;
 int main()
 {
-	int n;
-	cin>>n;
-	int count=0;
-	while(n/10>=1)
-	{
-		int sum=0;
-		int t=n;
-		while(t>0)
+	string s;
+	cin>>s;
+	if(s.size()==1)
+	cout<<0<<endl;
+	else{
+    	       int n=0;
+		for(int i=0;i<s.size();i++)
+		n+=(int)(s[i]-'0');
+	
+		int count=1;
+		while(n/10>=1)
 		{
-			sum+=t%10;
-			t/=10;
+			int sum=0;
+			int t=n;
+			while(t>0)
+			{
+				sum+=t%10;
+				t/=10;
+			}
+			count++;
+			n=sum;
 		}
-		count++;
-		n=sum;
-	}
-	cout<<count<<endl;
+		cout<<count<<endl;
+	}	
 	return 0;
 }
